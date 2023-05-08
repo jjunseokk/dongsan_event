b84c988f989e388a0b3020705b36ed8e077c845c
jjunseokk <p.junseok53@gmail.com>
2023-05-08 17:55:59 +0900

    login page update

---
+++ src/pages/Join.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const Join = () =>{
+    return (
+        <div>
+            <h1>관리자 페이지입니다.</h1>
+        </div>
+    )
+}
+
+
+export default Join;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,30 @@
 import React from "react";
+import Logo from '../image/dongsan.png';
+import '../style/login.scss';
 
 
-
-const Login = () =>{
+const Login = () => {
     return (
-        <div>
-            <h1>로그인 페이지입니다.</h1>
+        <div className="login-container">
+            <img src={Logo} alt="logo" />
+
+            {/* ------------로그인 영역-------------------------------- */}
+            <div className="login-area">
+                <form className="form-area">
+                    <div className="input-area">
+                        <div style={{ width: '100%' }}>
+                            <span className="label">이름</span>
+                            <input type="text" name="name" />
+                        </div>
+                        <div style={{ width: '100%' }}>
+                            <span className="label">비밀번호</span>
+                            <input type="password" name="password" />
+                        </div>
+                    </div>
+                    <button class="learn-more" type="submit">로그인</button>
+                </form>
+            </div>
+            <p>회원가입</p>
         </div>
     )
 }
